798b01751000566b2915f41f42c2706672fc89ae
Tim Weber <tim.weber@aggiemail.usu.edu>
2017-04-15 22:47:14 +0000

    Having a problem with migrations

--- django_polls/settings.py
+++ django_polls/settings.py
@@ -32,6 +32,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
