e55ed82d1c99f6e413ecba3e3b3cf06208df9986
Tim Weber <tim.weber@aggiemail.usu.edu>
2017-04-16 20:04:55 +0000

    Using generic views.  Following tutorial.

--- polls/views.py
+++ polls/views.py
@@ -1,33 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect,Http404
 from django.core.urlresolvers import reverse
 from django.template import loader
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
     
-def results(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
-    return render(request, 'polls/results.html', {'question':question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
     
 def vote(request, question_id):
     try:
