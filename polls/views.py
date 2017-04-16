56159d6d8b7e2556b6a9b549dc70326aaacc8fbd
Tim Weber <tim.weber@aggiemail.usu.edu>
2017-04-16 02:31:56 +0000

    Filled out vote().  Following tutorial.

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
+from django.core.urlresolvers import reverse
 from django.template import loader
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -26,4 +27,18 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
     
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Does not Exist.")
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question':question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
